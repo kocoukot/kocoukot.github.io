f904873d203098faf26ab78569490d0233202559
Anton Kudryavtsev <kocou-kot@yandex.ru>
2023-01-23 21:19:48 +0500

    first commit

---
+++ variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "/Users/kotwhite/Desktop/work/docs_task_1_showhelp";
+var completed_var = "/Users/kotwhite/Desktop/work/docs_task_1_completed";
+var store_var = "/Users/kotwhite/Desktop/work/docs_task_1_store";
