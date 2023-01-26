8e924a14379ff0cecbec851792d9f26823333b4b
Anton Kudryavtsev <kocou-kot@yandex.ru>
2023-01-26 12:28:28 +0500

    new test added

--- variables.js
+++
@@ -1,3 +0,0 @@
-var showhelp_var = "/Users/kotwhite/Desktop/work/side/tests/потребности в МТР /docs_task_20_showhelp";
-var completed_var = "/Users/kotwhite/Desktop/work/side/tests/потребности в МТР /docs_task_20_completed";
-var store_var = "/Users/kotwhite/Desktop/work/side/tests/потребности в МТР /docs_task_20_store";
