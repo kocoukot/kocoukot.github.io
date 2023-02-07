7970197eabb0c7e8832d4e0572be65f969726f4e
Anton Kudryavtsev <kocou-kot@yandex.ru>
2023-02-07 18:27:22 +0500

    20 added

---
+++ variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_showhelp";
+var completed_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_completed";
+var store_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_store";
