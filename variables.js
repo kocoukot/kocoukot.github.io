f6990e117df1a46d1eca2bf0b9f1f73a7fcba77d
Anton Kudryavtsev <kocou-kot@yandex.ru>
2023-02-08 11:18:44 +0500

    cleared

--- variables.js
+++
@@ -1,3 +0,0 @@
-var showhelp_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_showhelp";
-var completed_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_completed";
-var store_var = "/Users/kotwhite/Desktop/work/side/test/mtr/docs_task_20_store";
