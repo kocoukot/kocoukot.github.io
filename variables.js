c7739c7a1060c3ec8ab5c5f5a2cee8475500729d
Anton Kudryavtsev <kocou-kot@yandex.ru>
2023-02-09 14:29:18 +0500

    clear

--- variables.js
+++
@@ -1,3 +0,0 @@
-var showhelp_var = "/Users/kotwhite/Desktop/work/side/test/atom/docs_task_23_showhelp";
-var completed_var = "/Users/kotwhite/Desktop/work/side/test/atom/docs_task_23_completed";
-var store_var = "/Users/kotwhite/Desktop/work/side/test/atom/docs_task_23_store";
