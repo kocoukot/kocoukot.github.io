4a84785b82bac8001e4ca6bf207c5673aeb69adc
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2023-10-05 13:50:17 +0500

    atom player fix

--- build_player_atom/variables.js
+++ build_player_atom/variables.js
@@ -1,3 +1,3 @@
-var showhelp_var = "player_task_showhelp";
-var completed_var = "player_task_completed";
-var store_var = "player_task_store";
+var showhelp_var = "player_task_showhelp_atom";
+var completed_var = "player_task_completed_atom";
+var store_var = "player_task_store_atom";
