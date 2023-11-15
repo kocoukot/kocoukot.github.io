4e5fe02a4f6705e30fd99b90674adc6ecf66b60b
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2023-11-15 11:18:03 +0500

    scrolls added

---
+++ tests/scroll_40/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "scrolling_showhelp_scroll_40";
+var completed_var = "scrolling_completed_scroll_40";
+var store_var = "scrolling_store_scroll_40";
---
+++ tests/scroll_54/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "scrolling_showhelp_scroll_54";
+var completed_var = "scrolling_completed_scroll_54";
+var store_var = "scrolling_store_scroll_54";
