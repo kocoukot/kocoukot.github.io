03a3230ce8e757d6ec8ce71ece19520962383606
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2023-11-18 16:09:03 +0500

    new dnd

---
+++ tests/dnd_15/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "dnd_task_showhelp_dnd_15";
+var completed_var = "dnd_task_completed_dnd_15";
+var store_var = "dnd_task_store_dnd_15";
---
+++ tests/dnd_37/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "dnd_task_showhelp_dnd_37";
+var completed_var = "dnd_task_completed_dnd_37";
+var store_var = "dnd_task_store_dnd_37";
