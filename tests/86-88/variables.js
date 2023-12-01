486a4e6915cc7e38d190dd090da3bff4dc0b9f68
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2023-12-01 19:56:46 +0500

    86/88 + 90/92 + 131

---
+++ tests/86-88/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "dnd_task_showhelp_86";
+var completed_var = "dnd_task_completed_86";
+var store_var = "dnd_task_store_86";
---
+++ tests/90-92/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "dnd_task_showhelp_90";
+var completed_var = "dnd_task_completed_90";
+var store_var = "dnd_task_store_90";
