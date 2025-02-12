1d90e2b8ed05e2c7b69598b5866b49de31da19dc
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2025-02-12 14:21:11 +0500

    returned

---
+++ tests/lada/bank_player/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "player_task_showhelp_player";
+var completed_var = "player_task_completed_player";
+var store_var = "player_task_store_player";
---
+++ tests/lada/task_2_69_knot/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "task_2_69_knot+_showhelp";
+var completed_var = "task_2_69_knot+_completed";
+var store_var = "task_2_69_knot+_store";
