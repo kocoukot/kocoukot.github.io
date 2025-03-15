bec003a15bdb4fc45162aef484a2909e4b874d6b
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2025-03-15 23:54:47 +0500

    dnd fix and gpn player

--- tests/lada/gpn/bank_player/variables.js
+++ tests/lada/gpn/bank_player/variables.js
@@ -1,3 +1,3 @@
-var showhelp_var = "player_task_showhelp_player";
-var completed_var = "player_task_completed_player";
-var store_var = "player_task_store_player";
+var showhelp_var = "player_task_showhelp";
+var completed_var = "player_task_completed";
+var store_var = "player_task_store";
