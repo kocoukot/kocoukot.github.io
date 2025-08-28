5212c11dd0229cafb63e3a49c37f348c3602b1c6
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2025-08-28 23:20:16 +0500

    nea

--- tests/lada/gpn/task_1_25_dnd_cards/variables.js
+++ tests/lada/gpn/task_1_25_dnd_cards/variables.js
@@ -1,3 +1,3 @@
-var showhelp_var = "task_1_25_dnd_showhelp_cards";
-var completed_var = "task_1_25_dnd_completed_cards";
-var store_var = "task_1_25_dnd_store_cards";
+var showhelp_var = "task_1_25_dnd_cards_showhelp";
+var completed_var = "task_1_25_dnd_cards_completed";
+var store_var = "task_1_25_dnd_cards_store";
