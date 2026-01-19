f29953f785063ff2241ab8f0b5cd0c85876e3c62
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2026-01-19 12:15:15 +0300

    115 fix

--- tests/dnd_115/variables.js
+++ tests/dnd_115/variables.js
@@ -1,3 +1,3 @@
-var showhelp_var = "dnd_two_with_zoom_showhelp";
-var completed_var = "dnd_two_with_zoom_completed";
-var store_var = "dnd_two_with_zoom_store";
+var showhelp_var = "115_showhelp";
+var completed_var = "115_completed";
+var store_var = "115_store";
