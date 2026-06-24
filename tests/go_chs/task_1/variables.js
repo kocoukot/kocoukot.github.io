7e35e89a4a243c0c7e706bde2442034e2e136d5c
Anton Kudryavtsev <kydryavtsev_a@flowwow.com>
2026-06-24 19:22:01 +0500

    go chs tasks

---
+++ tests/go_chs/task_1/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "task_1_showhelp";
+var completed_var = "task_1_completed";
+var store_var = "task_1_store";
---
+++ tests/go_chs/task_2/variables.js
@@ -0,0 +1,3 @@
+var showhelp_var = "task_2_showhelp";
+var completed_var = "task_2_completed";
+var store_var = "task_2_store";
